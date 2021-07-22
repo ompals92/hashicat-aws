12e5b8e2d22c34e39b264f035231d0999e0cd42a
ompals92 <ompals92@gmail.com>
2021-07-22 19:08:44 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/ompal-training/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = "lakshya"
-  prefix = "ompal"
+  bucket_prefix = "ompal"
+#  prefix = "ompal"
 }  
